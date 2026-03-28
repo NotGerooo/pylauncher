3b4a8d1fa23c169b27e1426aa1d1d16853a762ba
NotGerooo <geroniminlopezt@gmail.com>
2026-03-28 21:23:29 +0000

    Mar 28, 2026, 4:23 PM

--- gui/views/library_view.py
+++ gui/views/library_view.py
@@ -128,7 +128,7 @@ class LibraryView:
             "▶ Jugar", bgcolor=GREEN, color=TEXT_INV, height=34,
             style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=6)),
             on_click=lambda e, p=profile: self._open_launch_dialog(p),
-        ),
+        )
         )
 
         return ft.Container(
