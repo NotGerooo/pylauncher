4305a885c0bf100ef2f148d6afc2c6838315ba7d
NotGerooo <geroniminlopezt@gmail.com>
2026-03-30 04:43:55 +0000

    Mar 29, 2026, 11:43 PM

--- main.py
+++ main.py
@@ -2,23 +2,14 @@ import sys
 import os
 import ssl
 
-# SSL fix for PyInstaller bundled builds
 ssl._create_default_https_context = ssl._create_unverified_context
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-import traceback
 import flet as ft
 
 def main(page: ft.Page):
-    try:
-        from gui.app import App
-        App(page)
-    except Exception as e:
-        traceback.print_exc()
-        # También mostrarlo en pantalla
-        page.add(ft.Text(str(e), color="red", size=12))
-        page.add(ft.Text(traceback.format_exc(), color="red", size=10))
-        page.update()
+    from gui.app import App
+    App(page)
 
 ft.app(target=main)
