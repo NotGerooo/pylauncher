3dad25001f5ef3c7b5a239594db0f6bf5fbdbf7a
NotGerooo <geroniminlopezt@gmail.com>
2026-03-31 02:21:42 +0000

    Mar 30, 2026, 9:21 PM

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import sys
 import os
 import ssl
+import ctypes  # 👈 AÑADE ESTO
 
-# SSL fix debe ir ANTES de cualquier import del proyecto
+# SSL fix
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# Asegurar que el directorio raiz del proyecto este en el path
+# 🔥 APP ID (AQUÍ EXACTO)
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+    'geros.launcher.minecraft.1.0'
+)
+
+# Path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-# Ahora si importamos modulos del proyecto
 from utils.logger import setup_logger
 import flet as ft
 
